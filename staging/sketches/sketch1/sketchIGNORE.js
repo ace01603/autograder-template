103ccaca89c2f216181cb1de32a25c94038b6aea
Abi Evans <50892754+ace01603@users.noreply.github.com>
2025-05-06 17:29:59 +0100

    added eslint with basic config

--- staging/sketches/sketch1/sketchIGNORE.js
+++ staging/sketches/sketch1/sketchIGNORE.js
@@ -9,3 +9,15 @@ function draw() {
     fill(255);
     circle(width / 2, height / 2, 50);
 }
+
+function deleteMe() {
+    var a = "a"
+    if (10 == "10") {
+        console.log("true")
+    }
+    return 1
+}
+
+// To ignore
+// no-unused-vars (makes things like setup() and error mecause they're not called)
+// no-undef (marks things like createCanvas() as undefined)
